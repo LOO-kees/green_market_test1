f1f1595e02cfa270ee1ad85a3211dfc3592c9545
LOO-kees <tlswjs16@gmail.com>
2025-06-26 12:53:21 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // src/App.js
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';  // ← Routes, Route 추가
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import './style/reset.css';
 import './style/common.css';
@@ -12,9 +12,10 @@ import Main from './components/Main';
 import Login from './components/Login';
 import Register from './components/Register';
 import Cart from './components/Cart';
-import ItemDetail from './components/ItemDetail';
 import ProductPage from './components/ProductPage';
+import ItemDetail from './components/ItemDetail';
 import GoodsInsert from './components/GoodsInsert';
+import GoodsEdit from './components/GoodsEdit';  // 상품 수정 페이지 추가
 import Notice from './components/Notice';
 import NoticeCreate from './components/NoticeCreate';
 import NoticeUpdate from './components/NoticeUpdate';
@@ -22,15 +23,14 @@ import NoticeDetail from './components/NoticeDetail';
 import MemberUpdate from './components/MemberUpdate';
 import InquiryForm from './components/InquiryForm';
 import Qna from './components/Qna';
-// import OtherPage from './components/OtherPage';  // 필요하다면 추가
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
 
-      {/* 페이지 컨텐츠 영역 */}
       <Routes>
+        {/* 메인 */}
         <Route path="/" element={<Main />} />
 
         {/* 로그인/회원가입/카트 */}
@@ -38,11 +38,11 @@ function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/cart" element={<Cart />} />
 
-        {/* 제품 관련 */}
+        {/* 상품 관련 */}
         <Route path="/productpage" element={<ProductPage />} />
--       <Route path="/itemdetail" element={<ItemDetail />} />
-        <Route path="/goodsinsert" element={<GoodsInsert />} />
         <Route path="/products/:id" element={<ItemDetail />} />
+        <Route path="/goodsedit/:id" element={<GoodsEdit />} />  {/* 상품 수정 */}
+        <Route path="/goodsinsert" element={<GoodsInsert />} />
 
         {/* 고객센터 */}
         <Route path="/notice" element={<Notice />} />
@@ -54,8 +54,6 @@ function App() {
 
         {/* 회원정보 수정 */}
         <Route path="/member/update/:id" element={<MemberUpdate />} />
-
-        {/* <Route path="/other" element={<OtherPage />} /> */}
       </Routes>
 
       <Footer />
